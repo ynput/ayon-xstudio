416747e645efb6f7050ebec3ffdb2487094f4d65
Philippe Leprince <philippe@ynput.io>
2025-08-21 17:45:01 +0200

    Update package.py

--- package.py
+++ package.py
@@ -1,16 +1,16 @@
 # Required: lower case addon name e.g. 'deadline', otherwise addon
 #   will be invalid
-name = ""
+name = "xstudio"
 
 # Optional: Addon title shown in UI, 'name' is used by default e.g. 'Deadline'
-title = ""
+title = "xStudio"
 
 # Required: Valid semantic version (https://semver.org/)
 version = "0.0.0"
 
 # Name of client code directory imported in AYON launcher
 # - do not specify if there is no client code
-client_dir = None
+client_dir = "ayon_xstudio"
 
 # Version compatibility with AYON server
 # ayon_server_version = ">=1.0.7"
@@ -19,7 +19,9 @@ client_dir = None
 
 # Mapping of addon name to version requirements
 # - addon with specified version range must exist to be able to use this addon
-ayon_required_addons = {}
+ayon_required_addons = {
+    "core": ">0.3.2",
+}
 # Mapping of addon name to version requirements
 # - if addon is used in the same bundle, the version range must be valid
 ayon_compatible_addons = {}
